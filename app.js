664dbfccb7bfb71772b048bb3a0229517f4baaf8
Christian.Dickerman <chrisdickerman93@gmail.com>
2022-01-17 21:17:52 -0700

    heroku deployment ready

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// app.get('/', function(req, res){
-//   res.send('./public/index.js');
-// });
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/main.html'))
 })
@@ -28,7 +24,4 @@ app.get('/picture2', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
